0175c2503f8ec84cfb5911bd4a8aa2259cbaefd9
장이삭 <95667521+op109@users.noreply.github.com>
2023-09-01 19:19:39 +0800

    Update 디스코드챗봇.py

--- 챗봇생성/디스코드챗봇.py
+++ 챗봇생성/디스코드챗봇.py
@@ -6,8 +6,9 @@ from konlpy.tag import Okt
 from collections import Counter
 import asyncio
 import pandas as pd
-TOKEN = '본인의 봇에맞는 토큰'
-CHANNEL_ID = '본인이 만든 대화 채널(서버 만들면 디폴드로 만들어진다.)'
+TOKEN = '본인의 봇 토큰'
+CHANNEL_ID = '본인의 서버를 만들고 난 후 생기는 기본 채팅 채널의 아이디'
+
 
 def load_dataframe():
     return pd.read_csv("아모레크롤링_스킨케어_완료.csv")
@@ -31,6 +32,13 @@ class MyClient(discord.Client):
         super().__init__(*args, **kwargs)
         self.keyword_mode_per_user = {} 
 
+    def find_matching_titles(self, title):
+        df = load_dataframe()
+        keywords = title.split()
+        condition = lambda x: all(keyword.lower() in x.lower() for keyword in keywords)
+        matching_titles = [t for t in df['상품명'].unique().tolist() if condition(t)]
+        return matching_titles
+
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -49,35 +57,53 @@ class MyClient(discord.Client):
             await self.close()  
             return
         
+
         if message.content.startswith('!chatbot'):
             await message.channel.send("상품 리뷰 요약 챗봇입니다. 상품명을 입력해 주세요.")
             while True:
                 msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-                if len(title) > 1:
-                    await self.chatbot(message, title)
-                    break
-                elif msg.content == 'q':  
+                
+                if msg.content == 'q':
                     await msg.channel.send('대화를 종료합니다.')
                     await self.close()  
                     return
+                
+                title = msg.content.strip()
+                
+                if len(title) > 1:
+                    matching_titles = self.find_matching_titles(title)
+                    if len(matching_titles) == 0:
+                        await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
+                        continue  # Continue the loop to keep asking for a valid product name.
+                    else:
+                        await self.chatbot(message, title)
+                        break
                 else:
                     await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
             self.keyword_mode_per_user[message.author.id] = False
             return
 
+
         elif message.content.startswith('!keyword'):
             await message.channel.send("키워드 추출 챗봇입니다. 상품명을 입력해 주세요.")
             while True:
                 msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-                if len(title) > 1:
-                    await self.keyword_bot(message, title)
-                    break
-                elif msg.content == 'q':  
+                
+                if msg.content == 'q':
                     await msg.channel.send('대화를 종료합니다.')
                     await self.close()  
                     return
+                
+                title = msg.content.strip()
+                
+                if len(title) > 1:
+                    matching_titles = self.find_matching_titles(title)
+                    if len(matching_titles) == 0:
+                        await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
+                        continue  # Continue the loop to keep asking for a valid product name.
+                    else:
+                        await self.keyword_bot(message, title)
+                        break
                 else:
                     await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
             self.keyword_mode_per_user[message.author.id] = False
@@ -128,30 +154,18 @@ class MyClient(discord.Client):
     async def chatbot(self, message, title):
         self.keyword_mode_per_user[message.author.id] = True
         df = load_dataframe()
+        
+        keywords = title.split()
+        condition = lambda x: all(keyword.lower() in x.lower() for keyword in keywords)
+        matching_titles = [t for t in df['상품명'].unique().tolist() if condition(t)]
+        
+        if len(matching_titles) == 0:
+            await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
+            return
+        elif len(title) == 1:
+            await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
+            return
 
-        while True:
-            keywords = title.split()
-            condition = lambda x: all(keyword.lower() in x.lower() for keyword in keywords)
-            matching_titles = [t for t in df['상품명'].unique().tolist() if condition(t)]
-
-            if len(matching_titles) == 0:
-                await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
-                await asyncio.sleep(0.5)  # 적절한 대기 시간
-
-                msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-            elif len(title) == 1:
-                await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
-                await asyncio.sleep(0.5)  # 적절한 대기 시간
-
-                msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-            elif msg.content == 'q':  
-                await msg.channel.send('대화를 종료합니다.')
-                await self.close()  
-                return
-            else:
-                break
 
         response = "일치하는 상품명 목록:\n"
         for idx, matching_title in enumerate(matching_titles, start=1):
@@ -164,6 +178,11 @@ class MyClient(discord.Client):
 
         try:
             msg = await self.wait_for('message', timeout=30.0, check=check)
+            if msg.content == 'q':  
+                await msg.channel.send('대화를 종료합니다.')
+                await self.close()  
+                return
+            
             selected_idx = int(msg.content) - 1
             selected_title = matching_titles[selected_idx]
             
@@ -171,7 +190,7 @@ class MyClient(discord.Client):
             reviews_text = ' '.join(reviews)
             
             inputs = tokenizer(reviews_text, return_tensors="pt", max_length=1024, truncation=True)
-            summary_ids = model.generate(inputs["input_ids"], num_beams=4, min_length=40, max_length=200, early_stopping=True)
+            summary_ids = model.generate(inputs["input_ids"], num_beams=10, min_length=50, max_length=150, early_stopping=True)
             summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
             self.keyword_mode_per_user[message.author.id] = False
@@ -186,32 +205,19 @@ class MyClient(discord.Client):
     async def keyword_bot(self, message, title):
         self.keyword_mode_per_user[message.author.id] = True
         df = load_dataframe()
-        matching_titles = df[df['상품명'].str.contains(title, case=False)]['상품명'].unique().tolist()
-
-        while True:
-            keywords = title.split()
-            condition = lambda x: all(keyword.lower() in x.lower() for keyword in keywords)
-            matching_titles = [t for t in df['상품명'].unique().tolist() if condition(t)]
-
-            if len(matching_titles) == 0:
-                await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
-                await asyncio.sleep(0.5)  # 적절한 대기 시간
-
-                msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-            elif len(title) == 1:
-                await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
-                await asyncio.sleep(0.5)  # 적절한 대기 시간
-
-                msg = await self.wait_for('message', timeout=30.0, check=lambda m: m.author == message.author and m.channel == message.channel)
-                title = msg.content.strip()
-            elif msg.content == 'q':  
-                await msg.channel.send('대화를 종료합니다.')
-                await self.close()  
-                return
-            else:
-                break
         
+        
+        keywords = title.split()
+        condition = lambda x: all(keyword.lower() in x.lower() for keyword in keywords)
+        matching_titles = [t for t in df['상품명'].unique().tolist() if condition(t)]
+        
+        if len(matching_titles) == 0:
+            await message.channel.send("해당 상품이 없습니다. 상품명을 똑바로 입력해 주세요.")
+            return
+        elif len(title) == 1:
+            await message.channel.send("최소 두 글자 이상의 쿼리를 입력해주세요.")
+            return
+
         response = "일치하는 상품명 목록:\n"
         for idx, matching_title in enumerate(matching_titles, start=1):
             response += f"{idx}. {matching_title}\n"
@@ -222,9 +228,13 @@ class MyClient(discord.Client):
             return m.author == message.author and m.channel == message.channel
 
         try:
-
             msg = await self.wait_for('message', timeout=30.0, check=check)
-            selected_idx = int(msg.content) - 1 
+            if msg.content == 'q':  
+                await msg.channel.send('대화를 종료합니다.')
+                await self.close()  
+                return
+            
+            selected_idx = int(msg.content) - 1
             selected_title = matching_titles[selected_idx]
 
             self.keyword_mode_per_user[message.author.id] = False
