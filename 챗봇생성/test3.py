cf8e060312a53a170e62b0e099bb7cf6093e146b
전승원 <jsw1255@gmail.com>
2023-09-25 09:52:54 +0900

    Add files via upload

--- 챗봇생성/test3.py
+++ 챗봇생성/test3.py
@@ -20,7 +20,7 @@ def reset_seeds(seed):
 SEED = 42
 reset_seeds(SEED)
 
-df = pd.read_csv("아모레크롤링_스킨케어.csv")
+df = pd.read_csv("아모레크롤링_스킨케어 (1).csv")
 
 # 제품별 평균 평점과 리뷰 수 계산
 product_rating_avg = df.groupby('상품명')['별점'].mean()
@@ -81,8 +81,47 @@ rmse = accuracy.rmse(predictions)
 id_to_user = {v: k for k, v in user_to_id.items()}
 id_to_product = {v: k for k, v in product_to_id.items()}
 
-def get_top_n_recommendations(predictions, n=5):
+# def get_top_n_recommendations(predictions, n=5):
+#     top_n = {}
+
+#     for uid, iid, true_r, est, _ in predictions:
+#         user_info = id_to_user[uid]
+#         product_name = id_to_product[iid]
+
+#         if user_info not in top_n:
+#             top_n[user_info] = []
+
+#         top_n[user_info].append((product_name, est))
+
+#     # 정렬, 중복 제거
+#     for user_info, user_ratings in top_n.items():
+#         user_ratings.sort(key=lambda x: x[1], reverse=True)
+#         seen = set()
+#         top_n_without_duplicates = []
+#         for product_name, est in user_ratings:
+#             if product_name not in seen:
+#                 seen.add(product_name)
+#                 top_n_without_duplicates.append((product_name, est))
+
+#         top_n[user_info] = top_n_without_duplicates[:n]
+
+#     return top_n
+
+def get_top_n_recommendations(df, n=5):
     top_n = {}
+    train_df, test_df = train_test_split(df,test_size=0.2,random_state=SEED,stratify=df['구매_클래스'])
+    reader = Reader(rating_scale=(0, 5))
+    train_data_surprise = Dataset.load_from_df(train_df[['user_id', 'product_id', '가중평점']], reader)
+    trainset = train_data_surprise.build_full_trainset()
+
+    testset = [(row['user_id'], row['product_id'], row['가중평점']) for i, row in test_df.iterrows()]
+
+    best_params = {'n_epochs': 100, 'lr_all': 0.005, 'reg_all': 0.2}
+    
+    model = SVD(n_epochs=best_params['n_epochs'], lr_all=best_params['lr_all'], reg_all=best_params['reg_all'],random_state=SEED)
+    model.fit(trainset)
+
+    predictions = model.test(testset)
 
     for uid, iid, true_r, est, _ in predictions:
         user_info = id_to_user[uid]
@@ -116,9 +155,9 @@ def get_unrated_items(user, df):
     unrated_items = all_items - rated_items
     return unrated_items
 
-user_recommendations_with_rated = get_top_n_recommendations(predictions, n=5)
+# user_recommendations_with_rated = get_top_n_recommendations(predictions, n=5)
 
-def content_based_recommendation_with_weights(age, gender, skin_type, skin_trouble, top_n=5, weight=0.1):
+def content_based_recommendation_with_weights(age, gender, skin_type, skin_trouble, top_n=5, weight=0.1, df=df):
     # 사용자 정보와 일치하는 리뷰 데이터 필터링
     filtered_df = df[(df['나이'] == age) & (df['성별'] == gender) &
                      (df['피부타입'] == skin_type) & (df['피부트러블'] == skin_trouble)]
@@ -140,7 +179,7 @@ def content_based_recommendation_with_weights(age, gender, skin_type, skin_troub
 
     return recommended_products
 
-def recommend_products_for_user(age, gender, skin_type, skin_trouble, top_n=5):
+def recommend_products_for_user(age, gender, skin_type, skin_trouble, top_n=5, df=df):
     # 가상 유저 ID를 생성
     virtual_user = f"{age},{gender},{skin_type},{skin_trouble}"
 
@@ -155,10 +194,11 @@ def recommend_products_for_user(age, gender, skin_type, skin_trouble, top_n=5):
 
     # 리뷰 수 상위 20% 이하인 경우 라이트 유저로 판단
     if user_review_count <= heavy_user_threshold:
-        return content_based_recommendation_with_weights(age, gender, skin_type, skin_trouble, top_n=top_n)
+        return content_based_recommendation_with_weights(age, gender, skin_type, skin_trouble, top_n=top_n, df=df)
     else:
         user_id = user_to_id[virtual_user]
         # CF 기반 추천 수행
+        user_recommendations_with_rated = get_top_n_recommendations(df, n=5)
         user_recommendations = user_recommendations_with_rated.get(virtual_user, [])
         recommended_products = [product_name for product_name, _ in user_recommendations[:top_n]]
         return recommended_products
@@ -257,6 +297,7 @@ if st.sidebar.checkbox("상품명 입력"):
         
         # 비슷한 상품 보기 선택
         if st.checkbox("비슷한 상품을 보여드릴까요?"):
+            # new_df = get_new_df(df)
             new_df = pd.read_csv("new_df.csv")
             input_keywords_str = new_df[new_df['상품명'] == selected_title]['키워드'].iloc[0]
             if not isinstance(input_keywords_str, str):
@@ -288,13 +329,15 @@ if st.sidebar.checkbox("고객타입 입력"):
     age = st.sidebar.selectbox("나이",["10대","20대","30대","40대","50대 이상"])
     skintype = st.sidebar.selectbox("피부타입",["복합성","건성","수분부족지성","지성","중성","극건성"])
     skintrouble = st.sidebar.selectbox("피부트러블",["민감성","건조함","탄력없음","트러블","주름","모공","칙칙함","복합성"])
-    
+    sub_category = st.sidebar.selectbox("상품소분류", df['상품소분류'].unique())
+
     if 'recommend_list' not in st.session_state:
         st.session_state['recommend_list'] = []
 
     if st.sidebar.button("추천받기"):
         st.header(f"{gender}, {age}, {skintype}, {skintrouble} 타입 고객님께 추천하는 제품입니다.")
-        recommend_list = recommend_products_for_user(age, gender, skintype, skintrouble)
+        filtered_df = df[df['상품소분류'] == sub_category]
+        recommend_list = recommend_products_for_user(age, gender, skintype, skintrouble,df=filtered_df)
         st.session_state['recommend_list'] = recommend_list
         for rec in st.session_state['recommend_list']:
             st.write(rec)
